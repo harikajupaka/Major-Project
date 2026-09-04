b29a29c6c0ea3455aedb8751f197c10f1e3a867c
harikajupaka <2403a51l31@sru.edu.in>
2026-09-05 02:26:39 +0530

    Update backend API URL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': 'http://localhost:8081'
+      '/api': 'https://major-project-frontend-qenj.onrender.com'
     }
   }
 })
